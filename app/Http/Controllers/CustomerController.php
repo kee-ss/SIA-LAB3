a518208023146f429ecac81117a20756c7451c3d
kee-ss <kellyanggaon26@gmail.com>
2026-04-21 15:39:38 +0800

    Add files via upload

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = Customer::all();
+        return view('customers.index', compact('customers'));
+    }
+
+    public function create()
+    {
+        return view('customers.create');
+    }
+ 
+    public function show(Customer $customer)
+    {   
+        return view('customers.show', compact('customer'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'gender' => 'required',
+            'dob' => 'required|date',
+            'address' => 'required',
+        ]);
+
+        Customer::create($request->all());
+
+        return redirect()->route('customers.index');
+    }
+
+    public function edit(Customer $customer)
+    {
+        return view('customers.edit', compact('customer'));
+    }
+
+    public function update(Request $request, Customer $customer)
+    {
+        $request->validate([
+            'name' => 'required',
+            'gender' => 'required',
+            'dob' => 'required|date',
+            'address' => 'required',
+        ]);
+
+        $customer->update($request->all());
+
+        return redirect()->route('customers.index');
+    }
+
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return redirect()->route('customers.index');
+    }
+}
